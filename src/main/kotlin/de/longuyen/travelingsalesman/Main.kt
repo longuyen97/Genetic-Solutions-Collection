0fb279e04a0c3e4b8bad4735e3a6028d1a23a0d5
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-12 12:42:43 +0200

    Update README

--- src/main/kotlin/de/longuyen/travelingsalesman/Main.kt
+++ src/main/kotlin/de/longuyen/travelingsalesman/Main.kt
@@ -3,7 +3,7 @@ package de.longuyen.travelingsalesman
 const val SANITY_CHECK = false
 
 fun main(){
-    val graph = CompleteGraph(10, 50, 100)
+    val graph = CompleteGraph(20, 50, 100)
     val population = Population(100, graph)
     population.fitAndSort()
     println(graph)
