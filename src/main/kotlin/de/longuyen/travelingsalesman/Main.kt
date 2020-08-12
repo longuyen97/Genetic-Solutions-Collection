f91a9fee0acebc9ba6dd6fd1a22aeee1d84876fa
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-12 12:45:10 +0200

    Solution

--- src/main/kotlin/de/longuyen/travelingsalesman/Main.kt
+++ src/main/kotlin/de/longuyen/travelingsalesman/Main.kt
@@ -3,13 +3,15 @@ package de.longuyen.travelingsalesman
 const val SANITY_CHECK = false
 
 fun main(){
-    val graph = CompleteGraph(20, 50, 100)
+    val graph = CompleteGraph(30, 50, 100)
     val population = Population(100, graph)
     population.fitAndSort()
+    val before = System.currentTimeMillis()
     println(graph)
-    for(i in 0 until 1000){
+    for(i in 0 until 10000){
         population.evolve()
         population.fitAndSort()
     }
-    println("Genetic algorithm's solution: Shortest path ${population.fittest().fitness} - Longest path ${population.worst().fitness}")
+    val after = System.currentTimeMillis()
+    println("Genetic algorithm's solution after ${(after - before) / 1000} seconds: Shortest path ${population.fittest().fitness} - Longest path ${population.worst().fitness}")
 }
